8a10ccff3ccf6cde14e359db34697dded4a481ac
StefanMaring <smaring@ziggo.nl>
2021-04-14 11:29:25 +0200

    heel veel dingen

--- header.php
+++ header.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>header</title>
+    <link rel="stylesheet" href="css/parts/header.css">
+    <link rel="stylesheet" href="css/parts/reset.css">
+    <title>Header</title>
 </head>
 <body>
     <header>
@@ -16,10 +17,10 @@
             <nav class="menu">
                 <div class="menu">
                     <ul>
-                         <li class="navitem"><a href="">Home</a></li>  
-                         <li class="navitem"><a href="">Items</a></li>
-                         <li class="navitem"><a href="">Over</a></li>
-                         <li class="navitem"><a href="">Contact</a></li>  
+                         <li class="navitem"><a class="nav-item" href="#">Home</a></li>  
+                         <li class="navitem"><a class="nav-item" href="#">Items</a></li>
+                         <li class="navitem"><a class="nav-item" href="#">Over</a></li>
+                         <li class="navitem"><a class="nav-item" href="#">Contact</a></li>  
                     </ul>
                 </div>
             </nav>
