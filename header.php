cbed903e4c40aa67c47e94b852fbd30433d652df
StefanMaring <smaring@ziggo.nl>
2021-04-14 10:56:34 +0200

    styling

--- header.php
+++ header.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>header</title>
 </head>
 <body>
-    <div class="header">
-        
-    </div>
+    <header>
+        <div class="header">
+            <div class="logo">
+                <h2>BunnyWiki</h2>
+            </div>
+            <nav class="menu">
+                <div class="menu">
+                    <ul>
+                         <li class="navitem"><a href="">Home</a></li>  
+                         <li class="navitem"><a href="">Items</a></li>
+                         <li class="navitem"><a href="">Over</a></li>
+                         <li class="navitem"><a href="">Contact</a></li>  
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
