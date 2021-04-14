15948a6c5d860b7caf160f24d77f7b844e57a222
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-14 12:09:54 +0200

    j
    j

--- footer/footer.php
+++ footer/footer.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../css/footer.css">
 <div class="footer">
     <ul>
-    <p class="footer_text">&copy Gemaakt door (Stefan Maring, Jinne Freijje, Wesley Roose)
+    <p class="footer_text">&copy Gemaakt door (Stefan Maring, Jinne Freije, Wesley Roose)
     </ul>
 </div>
 
