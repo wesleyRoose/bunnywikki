8fd068536fa867463eb7a53c560b0a634f966e96
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-14 11:29:11 +0200

    gras
    gras

--- footer/footer.php
+++ footer/footer.php
@@ -1,6 +1,8 @@
-<link rel="stylesheet" href="../assets/css/footer.css">
-<div>
+<link rel="stylesheet" href="../css/footer.css">
+<div class="footer">
     <ul>
-    <li
+
+    <p class="footer_text">&copy Gemaakt door (Stefan Maring, Jinne Freijje, Wesley Roose)
+    <p class="footer_text">voor bunny wiki
     </ul>
 </div>
