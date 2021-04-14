578018b27c2127ab4609e876faaa5d122d357819
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-14 12:08:39 +0200

    kleur
    kleur

--- footer/footer.php
+++ footer/footer.php
@@ -2,7 +2,6 @@
 <div class="footer">
     <ul>
     <p class="footer_text">&copy Gemaakt door (Stefan Maring, Jinne Freijje, Wesley Roose)
-    <p class="footer_text">voor bunny wiki
     </ul>
 </div>
 
