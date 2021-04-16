f0dce7269884f48a9fe20326465b1559fe2f714f
StefanMaring <smaring@ziggo.nl>
2021-04-16 11:36:24 +0200

    .

--- footer/footer.php
+++ footer/footer.php
@@ -1,4 +1,3 @@
-    <link rel="stylesheet" href="../css/footer.css">
     <div class="footer">
         <ul>
         <p class="footer_text">&copy; Gemaakt door (Stefan Maring, Jinne Freije, Wesley Roose)
