1f91a3aa5742d6aa0d421842fd649a75c66c9ee9
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-20 09:17:47 +0200

    foo
    foto

--- home/home.php
+++ home/home.php
@@ -1,41 +1,19 @@
 <link rel="stylesheet" href="../css/home.css">
+<link rel="script" href="../js/home.js">
 
-<p>Slideshow 2:</p>
 <div class="slideshow-container">
   <div class="mySlides2">
-    <img src="konijn1.jpg" style="width:100%">
+    <img src="konijn1.jpg" alt="konijn1" style="width:100%">
   </div>
 
   <div class="mySlides2">
-    <img src="konijn1.jpg" style="width:100%">
+    <img src="konijn1.jpg" alt="konijn2" style="width:100%">
   </div>
 
   <div class="mySlides2">
-    <img src="konijn1.jpg" style="width:100%">
+    <img src="gras-1.png" alt="konijn3" style="width:100%">
   </div>
 
   <a class="prev" onclick="plusSlides(-1, 1)">&#10094;</a>
   <a class="next" onclick="plusSlides(1, 1)">&#10095;</a>
 </div>
-
-<script>
-var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
-}
-</script>
