623590270f893c51a8512daa54add838c784a436
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-20 09:49:44 +0200

    code
    code

--- home/home.php
+++ home/home.php
@@ -18,8 +18,8 @@
   <a class="prev" onclick="plusSlides(-1, 1)">&#10094;</a>
   <a class="next" onclick="plusSlides(1, 1)">&#10095;</a>
 </div>
-
-<p>The items do not have to be displayed in the same order as they are written in the HTML code.</p>
+<br>
+<br>
 
 <div class="grid-container">
   <div class="item1">1</div>
