e2b1549ec42a7513b19d7c049ddf9e81589c79d8
StefanMaring <smaring@ziggo.nl>
2021-04-20 12:00:35 +0200

    Over ons
    Over ons pagina gemaakt

---
+++ over/over.php
@@ -0,0 +1,28 @@
+<?php include "../header/header.php"; ?>
+<head>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>BunnyWiki - Over</title>
+</head>
+
+<section class="over">
+    <div class="about">
+        <h4>Over Ons</h4>
+        <div class="about-text">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam id velit maximus, bibendum ligula at, facilisis elit. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan tellus lorem, non pulvinar nunc vulputate in. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Duis elit arcu, accumsan gravida ligula nec, placerat porta leo. Vestibulum ultrices orci eu augue pellentesque malesuada. 
+                Donec porta risus dictum, posuere arcu in, euismod eros. Ut sed magna mauris. Donec posuere mi eu enim sollicitudin lobortis. 
+                Pellentesque eu nisi pellentesque, venenatis leo in, convallis ipsum. 
+                Phasellus est augue, convallis sit amet maximus vitae, posuere ac ligula. Aenean lobortis dignissim dolor nec semper. 
+                Vestibulum mauris nisl, facilisis at egestas at, accumsan id enim. Sed dictum elementum dolor at fermentum. 
+                Proin pulvinar dignissim viverra. Etiam faucibus gravida consectetur.
+            </p>
+        </div>
+    </div>
+</section>
+
+
+<?php include "../footer/footer.php"; ?>
