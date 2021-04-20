dc88062508e281391851a527d92ca5c1a87f3989
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-20 09:47:44 +0200

    grid
    grid

--- home/home.php
+++ home/home.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../css/home.css">
+<link rel="stylesheet" href="../css/parts/home.css">
 <link rel="script" href="../js/home.js">
 
 
@@ -19,5 +19,14 @@
   <a class="next" onclick="plusSlides(1, 1)">&#10095;</a>
 </div>
 
+<p>The items do not have to be displayed in the same order as they are written in the HTML code.</p>
 
+<div class="grid-container">
+  <div class="item1">1</div>
+  <div class="item2">2</div>
+  <div class="item3">3</div>  
+  <div class="item4">4</div>
+  <div class="item5">5</div>
+  <div class="item6">6</div>
+</div>
 
