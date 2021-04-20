e84d870cba3eff6386cc84c4ffecf0739c2a95b0
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-04-20 09:54:07 +0200

    commit
    commit

--- home/home.php
+++ home/home.php
@@ -2,22 +2,7 @@
 <link rel="script" href="../js/home.js">
 
 
-<div class="slideshow-container">
-  <div class="mySlides2">
-    <img src="konijn1.jpg" alt="konijn11" style="width:100%">
-  </div>
 
-  <div class="mySlides2">
-    <img src="images/konijn1.jpg" alt="konijn22" style="width:100%">
-  </div>
-
-  <div class="mySlides2">
-    <img src="gras-1.png" alt="konijn33" style="width:100%">
-  </div>
-
-  <a class="prev" onclick="plusSlides(-1, 1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1, 1)">&#10095;</a>
-</div>
 <br>
 <br>
 
