6a140c850d93de91b50f5eb2015858aefddf2f81
JinneFreijee <jinne_freijee@hotmail.com>
2021-04-20 10:26:11 +0200

    Update login.php

---
+++ login/login.php
@@ -0,0 +1,47 @@
+<?php include "header/header.php"; ?>
+<!doctype html>
+<html lang="nl">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous">
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+        <!-- popper -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+        <!-- Bootstrap JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+        <title>p3-wk2-vraag1 </title>
+    </head>
+    <body>
+        
+    <h3> Login hieronder in</h3> 
+    <form action="check.php" method="POST">
+        <h4> Gebruikersnaam</h4>
+        <input class="login_user"  name="username" placeholder="Gebruikersnaam" type="text" >  </input>
+        <h4> Wachtwoord</h4>
+        <input placeholder="Wachtwoord" name="password" type="password">  </input>
+        <br>
+        <br>
+        <button> verzenden </button>
+    </form>
+    </body>
+    </html>
+
+<?php include "footer/footer.php"; ?>
