9b154bb29a0d304026dd734158a7cf52c6ac7012
JinneFreijee <jinne_freijee@hotmail.com>
2021-04-21 11:43:15 +0200

    Update footer.php

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,9 +1,38 @@
+<<<<<<< Updated upstream
 <div style="clear: both;"></div>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+<div style="clear: both;"></div>
+>>>>>>> b6841dfb1def62b8aef821b493b3f3cb3e2648ff
+=======
+<div style="clear: both;"></div>
+>>>>>>> b6841dfb1def62b8aef821b493b3f3cb3e2648ff
+>>>>>>> Stashed changes
     <div class="footer">
         <ul>
         <p class="footer_text">&copy; Gemaakt door (Stefan Maring, Jinne Freijee, Wesley Roose)
         </ul>
     </div>
+<<<<<<< Updated upstream
     <script src="../js/script.js"></script>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+    <script src="js/script.js"></script>
+=======
+    <script src="../js/script.js"></script>
+>>>>>>> e3bc3aa0bdaa8e59feb9ffa7973e25a10858bfba
+=======
+    <script src="../js/script.js"></script>
+>>>>>>> b6841dfb1def62b8aef821b493b3f3cb3e2648ff
+=======
+    <script src="../js/script.js"></script>
+>>>>>>> b6841dfb1def62b8aef821b493b3f3cb3e2648ff
+>>>>>>> Stashed changes
 </body>
 </html>
