e84a0fd2e083046b493bd381cd747a4b51b6ed87
StefanMaring <smaring@ziggo.nl>
2021-05-12 12:03:17 +0200

    Eigenlijk niks veranderd, maar ook weer wel

--- PHP/home.php
+++ PHP/home.php
@@ -6,7 +6,7 @@
 <div class="slideshow-container">
 
 <div class="mySlides fade">
-  <img src="../images/konijn1.jpg" class="images"  style="width:100%">
+  <img src="../images/konijn1.jpg" class="images"  style="width:100%;">
   <div class="text">Caption Text</div>
 </div>
 <div class="mySlides fade">
