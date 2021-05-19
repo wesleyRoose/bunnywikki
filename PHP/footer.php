ef66ed1d7b9739d42eae30a9bfe08e6d919222f4
StefanMaring <smaring@ziggo.nl>
2021-05-19 11:20:13 +0200

    update
    script voor jaar, en tekst die normaal alleen rechters uitspreken

--- PHP/footer.php
+++ PHP/footer.php
@@ -3,7 +3,9 @@
 
     <div class="footer">
         <ul>
-        <p class="footer_text">&copy; Gemaakt door (Stefan Maring, Jinne Freijee, Wesley Roose)
+        <p class="footer_text">&copy; Gemaakt door (Stefan Maring, Jinne Freijee, Wesley Roose) -
+            <script>var date = new Date(); var year = date.getFullYear(); document.write(year);</script> - Alle rechten voorbehouden
+        </p>
         </ul>
     </div>
 
