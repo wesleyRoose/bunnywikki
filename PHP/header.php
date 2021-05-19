a1a46b44c4ed52e9c39d02498606a9bd92e939f6
StefanMaring <smaring@ziggo.nl>
2021-05-19 12:03:33 +0200

    Update header.php

--- PHP/header.php
+++ PHP/header.php
@@ -3,11 +3,13 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="nl">
+<html lang="nl-NL">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="language" content="Dutch">
+    <meta http-equiv="content-language" content="nl">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" 
     integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
