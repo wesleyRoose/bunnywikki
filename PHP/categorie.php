bed3c62fd48c5b21516fc68ae29e80199d0670b6
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-05-19 10:20:51 +0200

    categorie
    categorie

--- PHP/categorie.php
+++ PHP/categorie.php
@@ -32,36 +32,43 @@
 <!-- grid box -->
 <div class="grid-container">
   <div class="item1">
-  <img src="../images/konijn1.jpg" class="grid_images">
-      <h6>Naam product</h6>
-      <br>
-      <p>Beschrijving product</p>
-      <br>
-      <p>Status</p>
+  <img src="../images/hok.jpg" class="grid_images">
+      <h6>Konijnen hok</h6>
       <br>
       <button class="button1">Click Me</button>
   </div>
   <div class="item3">
-      <img src="../images/konijn1.jpg" class="grid_images">
-      <h6>Naam product</h6>
-      <br>
-      <p>Beschrijving product</p>
-      <br>
-      <p>Status</p>
+      <img src="../images/beeldjes.jpg" class="grid_images">
+      <h6>Beeldjes</h6>
       <br>
       <button class="button1">Click Me</button>
   </div>  
   <div class="item4">
-      <img src="../images/konijn1.jpg" class="grid_images">
-      <h6>Naam product</h6>
+      <img src="../images/voer.jpg" class="grid_images">
+      <h6>konijnen voer</h6>
       <br>
-      <p>Beschrijving product</p>
+      <button class="button1">Click Me</button>
+</div>
+<div class="item2">
+  <img src="../images/hok.jpg" class="grid_images">
+      <h6>Konijnen hok</h6>
       <br>
-      <p>Status</p>
+      <button class="button1">Click Me</button>
+  </div>
+  <div class="item5">
+      <img src="../images/beeldjes.jpg" class="grid_images">
+      <h6>Beeldjes</h6>
+      <br>
+      <button class="button1">Click Me</button>
+  </div>  
+  <div class="item6">
+      <img src="../images/voer.jpg" class="grid_images">
+      <h6>konijnen voer</h6>
       <br>
       <button class="button1">Click Me</button>
 </div>
 
 </div>
+
 
 <?php include "footer.php"; ?>
