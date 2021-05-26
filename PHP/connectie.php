69c7c68f057565852116160f8d64c0db3ccaa128
JinneFreijee <jinne_freijee@hotmail.com>
2021-05-26 10:16:21 +0200

    .

--- PHP/connectie.php
+++ PHP/connectie.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 $dbServername = "localhost";
@@ -7,12 +6,5 @@ $dbServername = "localhost";
     $dbName = "bunnywiki";
 
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-=======
-<?php 
-
-session_start();
-error_reporting(0);
-date_default_timezone_set("Europe/Amsterdam");
->>>>>>> a1a46b44c4ed52e9c39d02498606a9bd92e939f6
 
 ?>
