429e4e3be3ad484702ed22db744f71051ed7ee8c
StefanMaring <smaring@ziggo.nl>
2021-06-17 11:41:19 +0200

    ..........
    lekker man

--- PHP/backoffice.php
+++ PHP/backoffice.php
@@ -9,6 +9,12 @@
             <input type="text" name="category" id="category" class="input_products"><br>
             <label>Timer:</label><br>
             <input type="time" name="timer_tijd" id="timer_tijd" class="input_products"><br>
+            <label>Minimumbod:</label><br>
+            <input type="number" name="minimum" id="minimum" class="input_products"><br>
+            <label>Einddatum:</label><br>
+            <input type="date" name="einddatum" id="einddatum" class="input_products"><br>
+            <label>Foto:</label><br>
+            <input type="file" name="foto" id="foto" class="input_products"><br>
             <label>Beschrijving</label><br>
             <textarea name="beschrijving" id="beschrijving" cols="30" rows="10" class="input_products" placeholder="Type hier"></textarea><br>
             <input type="submit" value="Verzenden" class="input_products">
