859efb37e4b2688b81416f036383f9c486e5f39c
StefanMaring <smaring@ziggo.nl>
2021-06-23 10:17:11 +0200

    Data.php
    Zorgt ervoor dat de producten in de tabel PRODUCTEN komen te staan in de database.

--- PHP/data.php
+++ PHP/data.php
@@ -1,6 +1,25 @@
 <?php 
 include "connectie.php";
 
+$id = "";
+$product_naam = $_POST["product_naam"];
+$categorie = $_POST["category"];
+$tijd = $_POST["time"];
+$minimumbod = $_POST["minimum"];
+$einddatum = $_POST["einddatum"];
+$foto = $_POST["foto"];
+$beschrijving = $_POST["beschrijving"];
 
+$query = "INSERT INTO product (";
+$query .= "product_naam, category, time, minimum, einddatum, foto, beschrijving";
+$query .= ") VALUES (";
+$query .= "'{$product_naam}','{$categorie}', '{$tijd}', '{$minimum}', '{$einddatum}', '{$foto}', '{$beschrijving}'";
+$query .= ")";
+
+$result = mysqli_query($conn, $query);
+$id = mysqli_insert_id($conn);
+
+header('location: ../backoffice.php');
+exit()
 
 ?>
