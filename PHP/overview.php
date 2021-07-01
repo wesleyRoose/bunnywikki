ad9029c445c7e47ea95b2565acbaa7b191d7991c
StefanMaring <smaring@ziggo.nl>
2021-07-01 09:19:59 +0200

    .

--- PHP/overview.php
+++ PHP/overview.php
@@ -1,8 +1,17 @@
+<?php include "connectie.php";?>
 <?php include "header.php";?>
 <main>
     <section class="overview">
         <div class="overview-holder">
-            
+            <h1>Overzicht van alle producten</h1>
+            <?php 
+                $result = mysqli_query($conn,"SELECT * FROM product ORDER BY ASC");
+                while ($rij = mysqli_fetch_array($result)) {
+                    echo '<div class="records"> ';
+                    echo '<div>' . $rij["product_naam"] . '</div>';
+                    echo '</div>';
+                }
+            ?>
         </div>
     </section>
 </main>
