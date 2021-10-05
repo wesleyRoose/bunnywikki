a11f0a79b7b413c1f6ef5c25d33a0a2b1ab1d4b5
StefanMaring <smaring@ziggo.nl>
2021-10-05 14:52:30 +0200

    Code Dropdown
    Code voor de dropdown is geschrapt aangezien deze niet meer bestaat

--- js/script.js
+++ js/script.js
@@ -5,18 +5,6 @@ function toggleMenu() {
     menu.classList.toggle("show");
 }
 
-// Functie voor dropdown 
-
-var dropdown = document.getElementById("dropdown");
-
-function dropDown() {
-    dropdown.classList.toggle("display");
-}
-
-function dontShow() {
-    dropdown.classList.remove("display");
-}
-
 // Functie voor Carousel
 
 var slideIndex = 1;
