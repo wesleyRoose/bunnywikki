2bec5dbdb9ee9aca83c8cbd432841098eb9dc0c4
StefanMaring <smaring@ziggo.nl>
2021-10-06 13:33:03 +0200

    Create account.php

---
+++ PHP/account.php
@@ -0,0 +1,9 @@
+<?php include_once "header.php";?>
+<main>
+    <section class="account">
+        <div class="account-page">
+
+        </div>
+    </section>
+</main>
+<?php include_once "footer.php";?>
