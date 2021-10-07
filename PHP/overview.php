355057c9572fa4a80245a1ec526f7f0749b7d103
StefanMaring <smaring@ziggo.nl>
2021-10-07 10:19:42 +0200

    pushhhhh

--- PHP/overview.php
+++ PHP/overview.php
@@ -1,17 +1,19 @@
-<?php include "connectie.php";?>
 <?php include "header.php";?>
 <main>
     <section class="overview">
         <div class="overview-holder">
             <h1>Overzicht van alle producten</h1>
+            <div class="products">
             <?php 
-                $result = mysqli_query($conn,"SELECT * FROM product ORDER BY ASC");
+                $result = mysqli_query($conn,"SELECT * FROM product");
                 while ($rij = mysqli_fetch_array($result)) {
                     echo '<div class="records"> ';
-                    echo '<div>' . $rij["product_naam"] . '</div>';
+                    echo '<div class="overview-item">' . $rij["product_naam"] . '</div>';
+                    echo '<div class="overview-item">' . $rij["beschrijving"] . '</div>';
                     echo '</div>';
                 }
             ?>
+            </div>
         </div>
     </section>
 </main>
