e33b8c7a8c9835aec408080523481058a68f2507
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-10-13 12:47:46 +0200

    ee
    eee

--- PHP/categorie.php
+++ PHP/categorie.php
@@ -18,19 +18,19 @@
 <!--grid menu-->
 <section class="overview">
         <div class="overview-holder">
-            <h1>Overzicht van alle producten</h1>
+
             <div class="products">
             <?php 
                 $result = mysqli_query($conn,"SELECT * FROM product");
                 while ($rij = mysqli_fetch_array($result)) {
                     echo '<div class="records"> ';
+                    echo '<div class="overview-item">' . $rij["foto"] . '</div>';
                     echo '<div class="overview-item">' . $rij["product_naam"] . '</div>';
-                    echo '<div class="overview-item">' . $rij["category"] . '</div>';
+                    echo '<div class="overview-item">' . $rij["beschrijving"] . '</div>';
                     echo '<div class="overview-item">' . $rij["time"] . '</div>';
                     echo '<div class="overview-item">' . $rij["minimum"] . '</div>';
-                    echo '<div class="overview-item">' . $rij["einddatum"] . '</div>';
-                    echo '<div class="overview-item">' . $rij["foto"] . '</div>';
-                    echo '<div class="overview-item">' . $rij["beschrijving"] . '</div>';
+                    echo '<div class="overview-item">' . $rij["einddatum"] . '</div>'; 
+                    echo '<div class="overview-item">' . $rij["category"] . '</div>';
                     echo '</div>';
                 }
             ?>
